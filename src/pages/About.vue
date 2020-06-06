8c61cc06da4ff146953261e7ab71988e028758df
= <nncl@live.com>
2020-06-06 17:37:44 -0300

    Initial commit & About page
    - About page:
      - Add title and metatags

---
+++ src/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>About</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageAbout',
+  meta: {
+    title: 'About',
+    meta: {
+      description: { name: 'description', content: 'This is about' }
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  meta: {
+    title: 'Home'
+  }
+}
+</script>
